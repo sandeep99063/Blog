64391e7c001be5358c37efe5d4c5d067eb86185c
Sandeep Kumar Singh <sandeep99063@gmail.com>
2015-07-13 22:09:59 +0530

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+   root 'pages#home'
+  get 'about' => 'pages#about'
+  
+ 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,5 +58,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-root 'people#index'
+
 end
